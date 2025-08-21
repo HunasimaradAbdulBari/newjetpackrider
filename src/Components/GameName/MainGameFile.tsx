d5671bb9c3c880579e6df133ad8185a7d70746e4
Bharathwaj V <bharathwajjvr@gmail.com>
2025-08-21 18:07:36 +0530

    Initial commit

---
+++ src/Components/GameName/MainGameFile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Your Main Game Renders here with all imports</div>
+  )
+}
+
+export default page
---
+++ src/app/(innerwebsite)/game/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+ import MainGameFile from "@/Components/GameName/MainGameFile"
+const page = () => {
+  return (
+    <div>
+        <MainGameFile />
+    </div>
+  )
+}
+
+export default page
